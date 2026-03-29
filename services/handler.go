37d32ae4983dd2c1e8824e7e236a38adfbba2313
Namchok <namchok.singhachai@gmail.com>
2026-03-29 21:34:53 +0700

    feat(migrations): expand initDefault to include detailed schema types and file paths

--- services/handler.go
+++ services/handler.go
@@ -66,14 +66,74 @@ func (h *Handler) DownDB(c echo.Context) error {
 func initDefault() {
 	migrations = []Migrations{
 		{
-			Type: "data",
+			Type: "schema_shared",
 		},
 		{
-			Type: "schema",
+			Type: "schema_content",
+		},
+		{
+			Type: "schema_identity",
+		},
+		{
+			Type: "schema_actor",
+		},
+		{
+			Type: "schema_media",
+		},
+		{
+			Type: "schema_attempts",
+		},
+		{
+			Type: "schema_streaks",
+		},
+		{
+			Type: "schema_quests",
+		},
+		{
+			Type: "schema_achievements",
+		},
+		{
+			Type: "schema_stats",
+		},
+		{
+			Type: "schema_leaderboard",
+		},
+		{
+			Type: "schema_social",
+		},
+		{
+			Type: "schema_economy",
+		},
+		{
+			Type: "schema_items",
+		},
+		{
+			Type: "schema_buffs",
+		},
+		{
+			Type: "schema_analytics",
+		},
+		{
+			Type: "data",
 		},
 	}
 	filePath = map[string]string{
-		"schema": "file://migration/schema",
+		"schema_shared":     "file://migration/schema_shared",
+		"schema_content":    "file://migration/schema_content",
+		"schema_identity":   "file://migration/schema_identity",
+		"schema_actor":      "file://migration/schema_actor",
+		"schema_media":      "file://migration/schema_media",
+		"schema_attempts":   "file://migration/schema_attempts",
+		"schema_streaks":    "file://migration/schema_streaks",
+		"schema_quests":     "file://migration/schema_quests",
+		"schema_achievements":"file://migration/schema_achievements",
+		"schema_stats":      "file://migration/schema_stats",
+		"schema_leaderboard":"file://migration/schema_leaderboard",
+		"schema_social":     "file://migration/schema_social",
+		"schema_economy":    "file://migration/schema_economy",
+		"schema_items":      "file://migration/schema_items",
+		"schema_buffs":      "file://migration/schema_buffs",
+		"schema_analytics":  "file://migration/schema_analytics",
 		"data":   "file://migration/data",
 	}
 }
